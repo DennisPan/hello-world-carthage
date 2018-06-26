bb3da8c50d0d4ae059937480e363d8f28fecfd7a
Dennis Pan <dpan@microsoft.com>
2018-06-26 12:49:50 -0700

    Initial commit

---
+++ hello-world-carthage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hello-world-carthage
+//
+//  Created by Dennis Pan on 6/26/18.
+//  Copyright © 2018 Dennis Pan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
